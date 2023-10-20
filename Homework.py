7bc7f84a061496ca8e9519d5c28e5c7c83605556
Your NamePaul <you@example.compaul.wirbelauer@stud.plus.ac.at>
2023-10-20 14:14:41 +0200

    Sonne

--- Homework.py
+++ Homework.py
@@ -3,6 +3,8 @@
 def check_weather(question):
     if question.lower() == "wie ist das wetter?":
         return "Das Wetter ist hervorragend. Da kann man nicht meckern"  # change
+    elif question.lower() == "scheint die sonne?":
+        return "Ja, vergiss die Sonnencreme nicht"  # add sun
     else:
         return "Diese Frage macht überhaupt keinen Sinn."
 
